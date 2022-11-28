7832390a793c860261c0ca3caeeb8c8a0172bba6
Abdulrahman Aminu <aminuabdulrahmandayo@gmail.com>
2022-11-29 00:34:07 +0100

    1. Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -30,56 +30,53 @@ class _MyAppState extends State<MyApp> {
       future: fetchSettings(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          return MultiProvider(
-            providers: [
-              ChangeNotifierProvider(
-                create: (_) => Numbers(),
-              ),
-              ChangeNotifierProvider(
-                create: (_) => settingsProvider,
-              )
-            ],
-            child: Consumer<SettingsProvider>(
-              builder: (context, settings, child) => MaterialApp(
-                title: 'Numbers Trivia App',
-                debugShowCheckedModeBanner: false,
-                theme: ThemeData(
-                  colorScheme: ColorScheme.fromSeed(
-                    seedColor: settings.colorValue,
-                    secondary: const Color.fromARGB(255, 8, 65, 112),
-                    background: const Color.fromARGB(255, 205, 195, 225),
-                  ),
-                ),
-                home: const MyHomePage(),
-              ),
-            ),
-          );
+          return MaterialAppWidget(settingsProvider: settingsProvider);
         }
-        return MultiProvider(
-          providers: [
-            ChangeNotifierProvider(
-              create: (_) => Numbers(),
-            ),
-            ChangeNotifierProvider(
-              create: (_) => settingsProvider,
-            )
-          ],
-          child: Consumer<SettingsProvider>(
-            builder: (context, settings, child) => MaterialApp(
-              title: 'Numbers Trivia App',
-              debugShowCheckedModeBanner: false,
-              theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(
-                  seedColor: settings.colorValue,
-                  secondary: const Color.fromARGB(255, 8, 65, 112),
-                  background: const Color.fromARGB(255, 205, 195, 225),
-                ),
-              ),
-              home: const MyHomePage(),
-            ),
-          ),
+        return Container(
+          color: Colors.white,
+          child: const Center(
+              child: CircularProgressIndicator(
+            color: Color(0xFF0C66B0),
+          )),
         );
       },
+    );
+  }
+}
+
+class MaterialAppWidget extends StatelessWidget {
+  const MaterialAppWidget({
+    Key? key,
+    required this.settingsProvider,
+  }) : super(key: key);
+
+  final SettingsProvider settingsProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Numbers(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => settingsProvider,
+        )
+      ],
+      child: Consumer<SettingsProvider>(
+        builder: (context, settings, child) => MaterialApp(
+          title: 'Numbers Trivia App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: settings.colorValue,
+              secondary: const Color.fromARGB(255, 8, 65, 112),
+              background: const Color.fromARGB(255, 205, 195, 225),
+            ),
+          ),
+          home: const MyHomePage(),
+        ),
+      ),
     );
   }
 }
